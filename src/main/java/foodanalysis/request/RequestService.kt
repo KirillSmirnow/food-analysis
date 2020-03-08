1f7d67cbe6cb1700309cf56ba5a3a5c2748393d2
Kirill Smirnov <smirnowku@gmail.com>
2020-03-08 20:55:17 +0300

    Work on RequestService interface

--- src/main/java/foodanalysis/request/RequestService.kt
+++ src/main/java/foodanalysis/request/RequestService.kt
@@ -1,8 +1,18 @@
 package foodanalysis.request
 
+import java.util.*
+
 interface RequestService {
+
+    fun getById(id: UUID): Request
 
     fun createOfText(text: String)
 
     fun createOfTelegramImage(imageId: String)
+
+    fun performImageDownloading(requestId: UUID)
+
+    fun performOcr(requestId: UUID)
+
+    fun performAnalysis(requestId: UUID)
 }
