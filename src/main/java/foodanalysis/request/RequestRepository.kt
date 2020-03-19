6e3bc33de97a61232bcaadb04b6ad52ab19ba3bf
Kirill Smirnov <smirnowku@gmail.com>
2020-03-19 14:15:50 +0300

    Implement request retrieval by request ID and client ID

--- src/main/java/foodanalysis/request/RequestRepository.kt
+++ src/main/java/foodanalysis/request/RequestRepository.kt
@@ -1,6 +1,11 @@
 package foodanalysis.request
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.*
 
-interface RequestRepository : MongoRepository<Request, UUID>
+interface RequestRepository : MongoRepository<Request, UUID> {
+
+    fun findByUserId(userId: UUID, pageable: Pageable): Page<Request>
+}
--- src/main/java/foodanalysis/request/RequestService.kt
+++ src/main/java/foodanalysis/request/RequestService.kt
@@ -1,10 +1,13 @@
 package foodanalysis.request
 
+import org.springframework.data.domain.Page
 import java.util.*
 
 interface RequestService {
 
     fun getById(id: UUID): Request
+
+    fun getByCurrentUser(page: Int): Page<Request>
 
     fun createOfText(text: String): Request
 
