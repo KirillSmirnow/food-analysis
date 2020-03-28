14dd4b9febe3d7e4d73984b15330a8a04fbb0cbf
Kirill Smirnov <smirnowku@gmail.com>
2020-03-28 18:14:17 +0300

    Implement substances import
    Secure all endpoint except 'requests'

--- src/main/java/foodanalysis/SecurityConfig.kt
+++ src/main/java/foodanalysis/SecurityConfig.kt
@@ -3,11 +3,17 @@ package foodanalysis
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 
 @Configuration
 open class SecurityConfig : WebSecurityConfigurerAdapter() {
 
     override fun configure(http: HttpSecurity) {
+        http.authorizeRequests()
+                .antMatchers("/requests/**").permitAll()
+                .anyRequest().authenticated()
+        http.httpBasic()
         http.csrf().disable()
+        http.sessionManagement().sessionCreationPolicy(STATELESS)
     }
 }
