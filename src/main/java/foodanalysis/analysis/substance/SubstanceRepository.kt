eb60c176a02b170fc3d961eb2229ae9fd34ebdb5
Kirill Smirnov <smirnowku@gmail.com>
2020-03-30 15:46:02 +0300

    Improve contents analysis #5

--- src/main/java/foodanalysis/analysis/substance/SubstanceRepository.kt
+++ src/main/java/foodanalysis/analysis/substance/SubstanceRepository.kt
@@ -3,4 +3,7 @@ package foodanalysis.analysis.substance
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.*
 
-interface SubstanceRepository : MongoRepository<Substance, UUID>
+interface SubstanceRepository : MongoRepository<Substance, UUID> {
+
+    fun findByCode(code: String): Substance?
+}
