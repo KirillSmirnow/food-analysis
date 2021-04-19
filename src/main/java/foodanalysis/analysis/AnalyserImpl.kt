afab253a12a7cde5e6da1a2fcf3ae54807d68a70
Kirill Smirnov <smirnowku@gmail.com>
2021-04-19 22:58:39 +0300

    Fix analysis: search for both Russian and English E's

--- src/main/java/foodanalysis/analysis/AnalyserImpl.kt
+++ src/main/java/foodanalysis/analysis/AnalyserImpl.kt
@@ -7,9 +7,14 @@ import org.springframework.stereotype.Service
 class AnalyserImpl(private val substanceRepository: SubstanceRepository) : Analyser {
 
     private val codeRegex = Regex("[EЕ][- ]?(\\d+[a-z]?)", RegexOption.IGNORE_CASE)
+    private val englishE = "E"
+    private val russianE = "Е"
 
     override fun analyse(text: String): Report {
-        val extractedCodes = codeRegex.findAll(text).map { "Е" + it.groupValues[1] }.distinct().toList()
+        val extractedCodes = codeRegex.findAll(text)
+            .distinct()
+            .flatMap { sequenceOf(englishE + it.groupValues[1], russianE + it.groupValues[1]) }
+            .toList()
         val detectedSubstances = extractedCodes.mapNotNull { substanceRepository.findByCode(it) }
         return Report(detectedSubstances)
     }
