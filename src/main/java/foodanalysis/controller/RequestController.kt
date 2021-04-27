9e42c335b7104961d1a1927551ffec5c4fa2956e
Kirill Smirnov <smirnowku@gmail.com>
2021-04-27 23:17:34 +0300

    Specify consuming media type of request creation method

--- src/main/java/foodanalysis/controller/RequestController.kt
+++ src/main/java/foodanalysis/controller/RequestController.kt
@@ -6,6 +6,7 @@ import foodanalysis.request.Request
 import foodanalysis.request.RequestService
 import foodanalysis.user.UserService
 import org.springframework.data.domain.Page
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
@@ -17,7 +18,7 @@ class RequestController(
     private val userService: UserService,
     private val fileService: FileService
 ) {
-    @PostMapping
+    @PostMapping(consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
     fun create(
         @RequestParam clientId: String,
         @RequestParam text: String?,
